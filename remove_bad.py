e4603fda5938e5f5719399d14cab57258b080344
kjain1810 <kunaljain.2000@yahoo.in>
2021-02-16 23:19:35 +0530

    Clean up day 1

---
+++ remove_bad.py
@@ -0,0 +1,13 @@
+import json
+
+with open("./init_vecs.json") as f:
+    results = json.load(f)
+
+results = results["vectors"]
+
+results = [result for result in results if result["val_error"] <= 1e20]
+
+results = {"vectors": results}
+
+with open("./init_vecs.json", "w") as f:
+    json.dump(results, f)
