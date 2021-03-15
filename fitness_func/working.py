d818a23ca3a30695a54bbc1810d050f7ddf1ef5c
samay-kothari <2001samay@gmail.com>
2021-03-16 00:38:34 +0530

    getting family of gens

--- fitness_func/working.py
+++ fitness_func/working.py
@@ -5,5 +5,5 @@ def fitness_lol(res, FACTOR=1.0):
     return abs(res[0] ** 2 - res[1] ** 2) + res[0] * res[1]
 
 
-def fitness(res, FACTOR=1.0):
+def fitness(res, FACTOR=0.5):
     return FACTOR * res[0] + res[1]
